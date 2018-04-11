90ed95a611c3c71528d0ba5ed83628d6065956c8
Yuda Aditya <yudaaditya98@gmail.com>
2018-04-11 08:40:59 +0700

    Tambah Plugin Bootstrap dan CI tapi di web kitanya belum ada make itu

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="Style.css">
+    <link rel="stylesheet"type="text/css" href="/bootstrap-4/css/bootstrap.min.css">
+
+    <title>KOMUNITED-Home</title>
+  </head>
+
+  <body>
+<div class="navbar">
+      <a class="active" href="index.php">Home</a>
+      <a href="calendar.html">Calendar</a>
+
+  <div class="dropdown">
+    <button class="dropbtn">Profile</button>
+      <div class="dropdown-content">
+        <a href="#">Founder Profile</a>
+        <a href="#">Community Profile</a>
+      </div>
+    </div>
+      <a href="category">Category</a>
+      <a href="tips.html">Tips</a>
+      <a href="about.html">About</a>
+</div>
+  </body>
+</html>
