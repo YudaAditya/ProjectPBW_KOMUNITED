f4576cf731a43f6322fc0417673fa0fa65f83cab
Yuda Aditya <yudaaditya98@gmail.com>
2018-04-14 23:01:54 +0700

    File terbaru Indexnya

--- index.php
+++ index.php
@@ -5,62 +5,25 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="stylesheet" href="bootstrap/4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="bootstrap/4.0/css/Style.css">
+  <link rel="stylesheet" href="css/Style.css">
 
 </head>
 <body>
+    <ul>
+      <li><a href="#" class="active">KOMUNITED</a></li>
+      <li><a href="#">Calendar</a></li>
+      <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Profile</a>
+      <div class="dropdown-content">
+          <a href="#">Founder Profile</a>
+          <a href="#">Community Profile</a>
+        </div>
+      <li><a href="#">About</a></li>
+      <input type="text" name="Search" >
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <!-- Brand -->
-  <a class="navbar-brand" href="#">KOMUNITED</a>
-
-  <!-- Links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" href="#">Calendar</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Category</a>
-    </li>
-
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Profile
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Founder Profile</a>
-        <a class="dropdown-item" href="#">Community Profile</a>
-      </div>
-    </li>
-
-    <!--Links-->
-    <li class="nav-item">
-      <a class="nav-link" href="#">About</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">     </a>
-    </li>
-
-  <form>
-    <input class="text-center" id="right" type="text" name="search" placeholder="">
-  </form>
-  <li class="nav-item">
-    <a class="nav-link" href="#">     </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="right" href="form.php">Sign Up</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" id="right" href="Login.php">Sign In</a>
-  </li>
-  </ul>
-
-</nav>
+      <li><a href="#">Sign Up</a></li>
+      <li><a href="#">Sign In</a></li>
+    </ul>
 
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,11 +1,68 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bootstrap/4.0/css/Style.css">
+</head>
 <body>
 
-  <p><b><h2> Login </h2></b></p>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand -->
+  <a class="navbar-brand" href="index.php">KOMUNITED</a>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Calendar</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Category</a>
+    </li>
+
+    <!-- Dropdown -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+        Profile
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Founder Profile</a>
+        <a class="dropdown-item" href="#">Community Profile</a>
+      </div>
+    </li>
+
+    <!--Links-->
+    <li class="nav-item">
+      <a class="nav-link" href="#">About</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">     </a>
+    </li>
+
+  <form  class="navbar-form">
+    <input class="text-center form-control" type="text" name="search" placeholder="">
+  </form>
+
+  <li class="nav-item">
+    <a class="nav-link" href="#">     </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="right" href="form.php">Sign Up</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="Login.php">Sign In</a>
+  </li>
+  </ul>
+</nav>
+
+
+
+  <p><b><h2 class="text-center"> Login </h2></b></p>
   <b>Username</b><br>
-  <input type="text" Name="username" size="25" placeholder="username"><br><br>
-  <b>Password</b><br>
+  <input type="text/file" Name="username" size="25" placeholder="username"><br><br>
+  <b class="text-center">Password</b><br>
   <input type="password" Name="sandi" size="25" placeholder="password"><br><br>
   <button type="submit">Login</button><br><br>
   <a href="">Daftar Akun</a>
